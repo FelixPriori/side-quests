d9d6130703726faa8b0f4809d3b0a7f6596185c1
FelixPriori <felixriouxsabourin@gmail.com>
2020-04-10 15:14:29 -0400

    reorganize file structure

---
+++ client/src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const classnames = require('classnames');
+
+
+export default function Button(props) {
+  const buttonClass = classnames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+  return (
+    <button className={buttonClass}>
+      {props.children}
+    </button>
+  );
+}
