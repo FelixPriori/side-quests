95a8a9c77ab25201fce909e5498fc160cfecdc46
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-04-10 14:48:30 -0400

    Login route and db helpers

---
+++ back-end/db/helpers.js
@@ -0,0 +1,24 @@
+const db = require('./index');
+const bcrypt = require('bcrypt');
+
+
+const correctPassword = function (email, password) {
+  const queryStr = `
+    SELECT password
+    FROM users
+    WHERE email = $1
+  `
+  return db.query(queryStr, [email])
+    .then(res => {
+      if (res.rows.length !== 0) {
+        return bcrypt.compareSync(password, res.rows[0].password)
+      } else {
+        return false
+      }
+    })
+};
+
+
+module.exports = {
+  correctPassword
+}
---
+++ back-end/routes/login.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+
+//Still need to npm install cookieSession and bcrypt
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
+
+//Helper functions
+const { correctPassword } = require('../db/helpers');
+
+module.exports = () => {
+  router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const loginError = "Error: Login credentials invalid. Please try again.";
+
+    if (email === '' || password === '') {
+      console.log(loginError);
+      res.send(loginError);
+    } else {
+      correctPassword(email, password)
+        .then(passwordMatch => {
+          if (passwordMatch) {
+            req.session.userId = email;
+            //UPDATE (UPDATE NAVBAR)
+          }
+        });
+    }
+
+  });
+
+
+  router.post('/logout', (req, res) => {
+    req.session = null;
+    //Update view
+  });
+
+
+
+
+  return router;
+};
