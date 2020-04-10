c5ec3347f0c6696f09057a92751bbb1f00242853
FelixPriori <felixriouxsabourin@gmail.com>
2020-04-10 18:27:53 -0400

    Implements temp logo

--- client/src/stories/button.stories.js
+++ client/src/stories/button.stories.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
 import Button from '../components/Button/Button';
 
 
@@ -8,4 +7,3 @@ export default {title: 'Button'}
 export const base = () => <Button>Base</Button>
 export const confirm  = () => <Button confirm>Confirm</Button>
 export const danger  = () => <Button danger>Cancel</Button>
-
---
+++ client/src/stories/logo.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Logo from '../components/Logo/Logo';
+
+export default {title: 'Logo'}
+export const base = () => <Logo></Logo>
