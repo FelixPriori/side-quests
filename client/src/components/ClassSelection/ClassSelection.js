c8e24c887527eb7a262319e2cd91fbbade6fee35
FelixPriori <felixriouxsabourin@gmail.com>
2020-04-12 11:55:40 -0400

    Class selection UI logic

--- client/src/components/ClassSelection/ClassSelection.js
+++ client/src/components/ClassSelection/ClassSelection.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../ClassSelection/ClassSelection.scss';
 
 
 export default function ClassSelection(props) {
+  const [ classItem, setClassItem ] = useState(null);
   const { classesData } = props;
+
+  const changeClass = name => {
+    const selectedClass = classesData.find(classData => classData.name === name);
+    setClassItem(selectedClass);
+  };
+
   const classList = classesData.map((classData, index) => {
+    const { name } = classData;
     return (
       <option 
         key={index}
-        value={classData.name}
+        value={name}
+        // onClick={() => console.log('clicked')}
+        //() => changeClass(name)
       >
-        {classData.name}
+        {name}
       </option>
     );
   })
   return (
     <main className="class__card">
-      <select id="classes" className="browser-default custom-select">
-        <option selected>Choose a class</option>
-        {classList}
-      </select>
+      <h2> Select a class </h2>
+      <div className="menu">
+        <select 
+          id="classes" 
+          className="browser-default custom-select"
+          onChange={e => changeClass(e.currentTarget.value)}
+        >
+          <option defaultValue>Choose a class</option>
+          {classList}
+        </select>
+      </div>
+        {classItem && 
+          <div className="content">
+            <img src={classItem.avatar}/>
+            <span>
+              <h3>{classItem.name}</h3>
+              <p>{classItem.description}</p>
+            </span>
+          </div>
+        }
     </main>
   );
 }
--- client/src/stories/class-select.stories.js
+++ client/src/stories/class-select.stories.js
@@ -5,15 +5,18 @@ import ClassSelection from '../components/ClassSelection/ClassSelection';
 const classesData = [
   {
     name: 'Rogue',
-    description: 'Rogues like to help people from the shadows by sneaking to the nearest store to deliver needed supplies'
+    description: 'Rogues like to help people from the shadows by sneaking to the nearest store to deliver needed supplies',
+    avatar: 'http://placekitten.com/150/150'
   },
   {
     name : 'Bard',
-    description: 'Bards loves to entertain. They specialize in letting the imagination run wild and help people think of happy thoughts for a while.'
+    description: 'Bards loves to entertain. They specialize in letting the imagination run wild and help people think of happy thoughts for a while.',
+    avatar: 'http://placekitten.com/150/150'
   },
   {
     name: 'Druid',
-    description: 'Druids love a companion and are happy to help your furry friends stay in tip top shape. Whether it\s babysitting, or simply giving the dog a walk, Druids are happy to help!'
+    description: 'Druids love a companion and are happy to help your furry friends stay in tip top shape. Whether it\s babysitting, or simply giving the dog a walk, Druids are happy to help!',
+    avatar: 'http://placekitten.com/150/150'
   }
 ];
 export default {title: 'Class Selection'}
