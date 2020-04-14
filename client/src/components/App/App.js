e0188064bcfcd769c8d6652661f2631ff12b55a5
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-04-14 13:47:26 -0400

    set up state

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -20,18 +20,24 @@ const SHOW = 'SHOW';
 const PROFILE = 'PROFILE';
 const EDIT = 'EDIT';
 
-const sessions = {id: true};
+const sessions = { id: true };
 const adventurer = true;
 const user = 'test';
 
 export default function App() {
-  const [ view, setView ] = useState( 
-    sessions.id 
-    ? adventurer
-      ? SHOW
-      : CREATE
-    : LOGIN 
+  const [view, setView] = useState(
+    sessions.id
+      ? adventurer
+        ? SHOW
+        : CREATE
+      : LOGIN
   );
+
+  const [state, setState] = useState({
+    classesProgressData: [],
+    classesData: [],
+    userData: []
+  });
 
   const changeView = (viewType) => {
     setView(viewType);
@@ -39,31 +45,31 @@ export default function App() {
 
   return (
     <div className="App">
-      { sessions.id 
+      {sessions.id
         ? <Navbar
-              user={user}
-              adventurer={adventurer}
-              onQuests={() => changeView(SHOW)}
-              onLogout={() => changeView(LOGIN)}
-              onCreate={() => changeView(CREATE)}
-              onLogout={() => changeView(LOGIN)}
-              onLogin={() => changeView(LOGIN)}
-              onRegister={() => changeView(REGISTER)}
-              onProgress={() => changeView(CLASSES)}
-              onProfile={() => changeView(PROFILE)}
-            />
-        : <Navbar 
-            onLogin={() => changeView(LOGIN)} 
-            onRegister={() => changeView(REGISTER)}
-          />
+          user={user}
+          adventurer={adventurer}
+          onQuests={() => changeView(SHOW)}
+          onLogout={() => changeView(LOGIN)}
+          onCreate={() => changeView(CREATE)}
+          onLogout={() => changeView(LOGIN)}
+          onLogin={() => changeView(LOGIN)}
+          onRegister={() => changeView(REGISTER)}
+          onProgress={() => changeView(CLASSES)}
+          onProfile={() => changeView(PROFILE)}
+        />
+        : <Navbar
+          onLogin={() => changeView(LOGIN)}
+          onRegister={() => changeView(REGISTER)}
+        />
       }
       <main>
-        {view === LOGIN && <LoginForm/>}
+        {view === LOGIN && <LoginForm />}
         {view === CLASSES && <AllClasses classesData={classesData} classesProgessData={classesProgessData} />}
-        {view === REGISTER && <RegisterForm/>}
-        {view === CREATE && <CreateQuestForm/>}
+        {view === REGISTER && <RegisterForm />}
+        {view === CREATE && <CreateQuestForm />}
         {view === SHOW && <ClassSelection classesData={classesData} classesProgessData={classesProgessData} />}
-        {view === PROFILE && <Profile onEdit={() => changeView(EDIT)} userData={userData}/>}
+        {view === PROFILE && <Profile onEdit={() => changeView(EDIT)} userData={userData} />}
         {view === EDIT && <RegisterForm userData={userData} onProfile={() => changeView(PROFILE)} />}
       </main>
     </div>
