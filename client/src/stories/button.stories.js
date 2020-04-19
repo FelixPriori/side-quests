749f4d4f4c12ee2579440e598aa816fbf01a5f07
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-04-19 10:32:29 -0400

    Google hangout button

--- client/src/stories/button.stories.js
+++ client/src/stories/button.stories.js
@@ -3,7 +3,6 @@ import Button from '../components/Button/Button';
 
 
 export default { title: 'Button' }
-
 export const navbar = () => <Button navbar>Login</Button>
 export const confirm = () => <Button confirm>Confirm</Button>
 export const danger = () => <Button danger>Cancel</Button>
---
+++ client/src/stories/google-hangout-button.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from '../components/Button/Button';
+
+
+export default { title: "GoogleHangoutButton" };
+
+export const button = () => <a href="https://hangouts.google.com/call/4vTdHBEPZQ6TnGAwr570AEEE?no_rd" target="_blank"><Button confirm>Hangout</Button></a>
