ef2896b0d2a8b531f02ce79701d0d2e4a6bae30e
Martin Carel <cawel00@yahoo.com>
2020-04-29 11:43:58 -0400

    fix bad merge

--- back-end/routes/badges.js
+++ back-end/routes/badges.js
@@ -1,25 +1,20 @@
-const express   = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-const { allBadges, getBadge } = require('../db/helpers');
+const { allBadges, getBadge } = require("../db/helpers");
 
 module.exports = () => {
-
   router.get("/badges", (req, res) => {
-
-    allBadges().then(result => {
+    allBadges().then((result) => {
       res.send(result);
     });
   });
 
   router.get("/badges/:id", (req, res) => {
-
-    getBadge(req.params.id).then(result => {
+    getBadge(req.params.id).then((result) => {
       res.send(result);
     });
   });
-
 
   return router;
 };
