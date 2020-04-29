2c73f34f9075adbbd051c4d1ae8818de118a8002
FelixPriori <felixriouxsabourin@gmail.com>
2020-04-29 10:33:01 -0400

    adds default avatar link instead of external url

--- back-end/routes/register.js
+++ back-end/routes/register.js
@@ -25,7 +25,7 @@ module.exports = () => {
       accountType = true;
     }
     if (!avatar) {
-      avatar = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png';
+      avatar = '/images/defaultAvatar.png';
     }
     if (!username || !email || !password) {
       res.status(401).send(EMPTY_ERROR)
