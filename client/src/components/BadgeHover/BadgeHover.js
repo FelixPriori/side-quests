ce70cfd66acaa43831356c74ea844acd33ff0045
Martin Carel <cawel00@yahoo.com>
2020-05-07 11:04:57 -0400

    fix all ESLint errors in BadgeHover.js

--- client/src/components/BadgeHover/BadgeHover.js
+++ client/src/components/BadgeHover/BadgeHover.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./BadgeHover.scss";
+import PropTypes from "prop-types";
 
+BadgeHover.propTypes = {
+  name: PropTypes.string.isRequired,
+  requirement: PropTypes.string.isRequired,
+};
 
-
-export default function BadgeHover(props) {
-
+export const BadgeHover = (props) => {
   return (
     <div className="badgeHover rounded">
       <h4>{props.name}</h4>
       <p>{props.requirement}</p>
     </div>
-  )
-}
+  );
+};
