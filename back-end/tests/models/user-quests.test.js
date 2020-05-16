7eec8b867f925ba41b2d622e8a102ff2375779a0
FelixPriori <felixriouxsabourin@gmail.com>
2020-05-16 16:22:51 -0400

    adds tests for user-quests NOT PASSING

---
+++ back-end/tests/models/user-quests.test.js
@@ -0,0 +1,54 @@
+const User = require("../../db/models/users");
+const Quest = require("../../db/models/quests");
+const faker = require("faker");
+// sequelizeClient.sync({ force: true, match: /_test$/ });
+
+describe("user relationships", () => {
+  it("can fetch the related quests", async () => {
+    // data creation
+    const villager = User.create({
+      username: faker.internet.userName(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      avatar: faker.internet.avatar(),
+      adventurer: false,
+      bio: faker.lorem.paragraph(),
+    });
+
+    const adventurer = User.create({
+      username: faker.internet.userName(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      avatar: faker.internet.avatar(),
+      adventurer: true,
+      bio: faker.lorem.paragraph(),
+    });
+
+    Quest.create({
+      name: faker.name.jobType(),
+      description: faker.lorem.paragraph(),
+      completed: false,
+      city: faker.address.city(),
+      classId: 1,
+      adventurerId: adventurer.id,
+      villagerId: villager.id,
+    });
+
+    Quest.create({
+      name: faker.name.jobType(),
+      description: faker.lorem.paragraph(),
+      completed: false,
+      city: faker.address.city(),
+      classId: 1,
+      villagerId: villager.id,
+    });
+    // test
+    const quests = User.findByPk(1).getQuests();
+    // expectations
+    expect(quests.length).toEqual(2);
+  });
+});
