d7312e3c7bb670b49f3c81487e8821bea2007a7a
Martin Carel <cawel00@yahoo.com>
2020-05-18 13:21:25 -0400

    add log trace so that we know what db we are connected to

--- back-end/environment.js
+++ back-end/environment.js
@@ -3,6 +3,9 @@ const path = require("path");
 const ENV = process.env.NODE_ENV || "development";
 const PATH = path.resolve(__dirname, "../back-end/.env." + ENV);
 
+console.log(`ENV: ${ENV}`);
+console.log(`Loading ${PATH}`);
+
 require("dotenv").config({ path: PATH });
 
 module.exports = ENV;
