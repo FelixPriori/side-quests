c82b650260023a7d1ff3c5b28ba27380723a697f
Martin Carel <cawel00@yahoo.com>
2020-05-18 13:20:28 -0400

    extract testDbConnection into file

---
+++ back-end/db/test_db_connection.js
@@ -0,0 +1,16 @@
+const sequelize = require("./sequelize");
+
+const testDbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      `Connection to '${process.env.DB_NAME}' db has been established successfully.`
+    );
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+//Note: To close connection use sequelize.close()
+
+module.exports.testDbConnection = testDbConnection;
