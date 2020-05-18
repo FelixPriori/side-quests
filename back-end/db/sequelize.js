b7e206731b05283b8d8178f8ec2f9dfb3b455d4f
Martin Carel <cawel00@yahoo.com>
2020-05-18 13:22:03 -0400

    remove useless interpolation

--- back-end/db/sequelize.js
+++ back-end/db/sequelize.js
@@ -1,12 +1,11 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
 const sequelize = new Sequelize(
-  `${process.env.DB_NAME}`,
-  `${process.env.DB_USER}`,
-  `${process.env.DB_PASS}`,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: `${process.env.DB_HOST}`,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   }
 );
