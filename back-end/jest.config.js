da6bb9789327552566d398d47f291df7ce31c9c6
Martin Carel <cawel00@yahoo.com>
2020-05-19 16:34:48 -0400

    remove dead code

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -1,15 +1,3 @@
 module.exports = {
   setupFilesAfterEnv: ["./tests/jest.setup.js"],
-  //   roots: ['<rootDir>/test', '<rootDir>/src'],
-  //   moduleFileExtensions: ['ts', 'js', 'json'],
-  //   transform: {
-  //     '^.+\\.(ts)$': 'ts-jest',
-  //   },
-  //   globals: {
-  //     'ts-jest': {
-  //       tsConfig: './jest.tsconfig.json',
-  //     },
-  //   },
-  //   testMatch: ['**/*.test.ts'],
-  //   testEnvironment: 'node',
 };
