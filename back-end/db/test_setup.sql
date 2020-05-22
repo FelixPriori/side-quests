9dcccf94e2ba63a068ed79473a487c06ec44b935
Martin Carel <cawel00@yahoo.com>
2020-05-22 10:01:24 -0400

    no longer seeding the test db

--- back-end/db/test_setup.sql
+++ back-end/db/test_setup.sql
@@ -1,12 +1,4 @@
 \i back-end/db/migrations/01_schema.sql
-\i back-end/db/seeds/01_users.sql
-\i back-end/db/seeds/02_classes.sql
-\i back-end/db/seeds/03_badges.sql
-\i back-end/db/seeds/04_assigned_badges.sql
-\i back-end/db/seeds/05_class_progress.sql
-\i back-end/db/seeds/06_quests.sql
-
-
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO quester;
 
