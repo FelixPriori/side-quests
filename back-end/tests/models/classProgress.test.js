0b4918a1315a144057a45ef46f49e965c8a15617
FelixPriori <felixriouxsabourin@gmail.com>
2020-05-24 09:59:01 -0400

    adds tests for class progress (fails)

---
+++ back-end/tests/models/classProgress.test.js
@@ -0,0 +1,24 @@
+const { createProgress, createAdventurer, createClass } = require("../seeds");
+const ClassProgress = require("../../db/models/classProgress");
+
+// test data
+let classInstance;
+let adventurer;
+let classProgress;
+
+describe("class progress model", () => {
+  beforeEach(async () => {
+    classInstance = await createClass();
+    adventurer = await createAdventurer();
+    classProgress = await createProgress({
+      adventurerId: adventurer.id,
+      classId: classInstance.id,
+    });
+  });
+  it("should have default values upon creation", async () => {
+    const newProgress = await ClassProgress.findByPk(classProgress.id);
+    expect(newProgress.level).toEqual(1);
+    expect(newProgress.experience_points).toEqual(0);
+    expect(newProgress.quest_count).toEqual(0);
+  });
+});
