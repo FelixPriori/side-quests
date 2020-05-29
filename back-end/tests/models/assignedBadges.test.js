3b3d949e93c94fb5c878c6a815e4c18efd0f80fe
FelixPriori <felixriouxsabourin@gmail.com>
2020-05-29 08:30:36 -0400

    writes tests for user.getBadges

--- back-end/tests/models/assignedBadges.test.js
+++ back-end/tests/models/assignedBadges.test.js
@@ -4,10 +4,14 @@ const {
   createBadge,
 } = require("../seeds");
 const AssignedBadge = require("../../db/models/assignedBadges");
+const User = require("../../db/models/users");
 
 let assignedBadge;
+let assignedBadge2;
 let adventurer;
 let badge;
+let badge2;
+let user;
 
 describe("assigned badges model", () => {
   beforeEach(async () => {
@@ -29,5 +33,31 @@ describe("assigned badges model", () => {
     const badgeForAdventurer = await AssignedBadge.findByPk(assignedBadge.id);
     const adventurerFromBadge = await badgeForAdventurer.getAdventurer();
     expect(adventurerFromBadge.id).toEqual(adventurer.id);
+  });
+});
+
+describe("For a given user", () => {
+  beforeEach(async () => {
+    user = await createAdventurer();
+    badge = await createBadge();
+    badge2 = await createBadge();
+    assignedBadge = await createAssignedBadge({
+      userId: user.id,
+      badgeId: badge.id,
+    });
+    assignedBadge2 = await createAssignedBadge({
+      userId: user.id,
+      badgeId: badge2.id,
+    });
+  });
+
+  it.only("can fetch the associated badges", async () => {
+    const userInstance = await User.findByPk(user.id);
+    const userBadges = await userInstance.getBadges();
+    expect(userBadges.length).toEqual(2);
+
+    const badgeIds = userBadges.map((b) => b.id).sort();
+    expect(badgeIds).toEqual([badge.id, badge2.id].sort());
+    console.log(assignedBadge2); // this is to avoid linter error cause it's declared but not used.
   });
 });
