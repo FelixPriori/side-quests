0a312e0868ee7d30b918128e6eb8bd03d3b1d7a3
Martin Carel <cawel00@yahoo.com>
2020-05-29 17:14:30 -0400

    remove useless code (since it is generated by sequelize)

--- back-end/db/models/assignedBadges.js
+++ back-end/db/models/assignedBadges.js
@@ -1,28 +1,8 @@
 const sequelize = require("../sequelize");
-const { DataTypes } = require("sequelize");
-const Badge = require("./badges");
-const User = require("./users");
 
 const AssignedBadge = sequelize.define(
   "assigned_badge",
-  {
-    badgeId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Badge,
-        key: "id",
-      },
-      allowNull: false,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: User,
-        key: "id",
-      },
-      allowNull: false,
-    },
-  },
+  {},
   {
     timestamps: false,
   }
