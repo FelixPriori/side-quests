47cf0d2615d2da43e82647d4084b5012ecc746a4
Martin Carel <cawel00@yahoo.com>
2020-05-29 17:47:03 -0400

    add test for 'badge belongs to many users' relationship

--- back-end/tests/models/badges.test.js
+++ back-end/tests/models/badges.test.js
@@ -1,8 +1,15 @@
-const { createClass, createBadge } = require("../seeds");
+const {
+  createClass,
+  createBadge,
+  createUser,
+  createAssignedBadge,
+} = require("../seeds");
 const Badge = require("../../db/models/badges");
 
 let classInstance;
 let badge;
+let user1;
+let user2;
 
 describe("badges model", () => {
   beforeEach(async () => {
@@ -14,5 +21,31 @@ describe("badges model", () => {
     const badgeForClass = await Badge.findByPk(badge.id);
     const classForBadge = await badgeForClass.getClass();
     expect(classForBadge.id).toEqual(badge.class_id);
+  });
+
+  describe("when badge has users", () => {
+    beforeEach(async () => {
+      user1 = await createUser();
+      user2 = await createUser();
+
+      // associate those 2 users to the badge
+      await createAssignedBadge({
+        userId: user1.id,
+        badgeId: badge.id,
+      });
+      await createAssignedBadge({
+        userId: user2.id,
+        badgeId: badge.id,
+      });
+    });
+
+    it("can fetch the associated users", async () => {
+      const userBadge = await Badge.findByPk(badge.id);
+      const users = await userBadge.getUsers();
+
+      expect(users.length).toEqual(2);
+      const userIds = users.map((b) => b.id).sort();
+      expect(userIds).toEqual([user1.id, user2.id].sort());
+    });
   });
 });
